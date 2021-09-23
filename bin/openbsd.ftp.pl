e7d035823546a2def68e7c6c85a1b2de49b10c3c
rwp <rwp.primary@gmail.com>
2021-09-23 17:03:31 +0400

    modifications, bin improvement

--- bin/openbsd.ftp.pl
+++ bin/openbsd.ftp.pl
@@ -4,6 +4,7 @@ use constant {
 HOST => 'ftp.usa.openbsd.org',
 DIRECTORY => '/pub/OpenBSD/snapshots/amd64',
 FILE => 'bsd',
+SYSCTL => 'kern.version',
 };
 use Net::FTP;
 use POSIX (); # method: mdtm
@@ -13,4 +14,11 @@ my $ftp = Net::FTP -> new(HOST);
 $ftp -> login();
 $ftp -> cwd(DIRECTORY);
 # say $_ for $ftp -> dir();
-say POSIX::strftime('%b %d, %Y (at %H:%M)', gmtime($ftp -> mdtm(FILE)));
+
+my $modification = POSIX::strftime('%b %d, %Y (at %H:%M)', gmtime($ftp -> mdtm(FILE)));
+say "m: $modification";
+
+my %kern;
+($kern{date}, $kern{time}, $kern{year}) = `sysctl -n @{[SYSCTL]}` =~ /(\w{3} \d{2}) (\d{2}:\d{2}):\d{2} \w{3} (\d{4})/;
+say "c: $kern{date}, $kern{year} (at $kern{time})";
+# say $sysctl;
